986d1fd8950e5d7ac28b229e188c2119db84d183
Marina Yeremina <marina.era121@mail.ru>
2024-02-28 22:59:28 +0500

    ввела первую часть кода

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите элементы массива, разделенные запятой:");
+        string input = Console.ReadLine();
+
+        string[] inputArray = input.Split(',');
+
+        string[] resultArray = FilterArray(inputArray);
+
+        Console.WriteLine("Результат:");
+        foreach (var item in resultArray)
+        {
+            Console.WriteLine(item);
+        }
+    }
